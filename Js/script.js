95b70abaa3ccb4a32fcf7edec922dee9ddfe6686
Kibet 1998 <ronoronald1998@gmail.com>
2021-04-18 14:56:57 +0300

    add development toggle effect

---
+++ Js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#designP").show();
+        $("#design").hide();
+    }); 
+    
+    $("#designP").click(function(){
+        $("#design").show();
+        $("#designP").hide();
+    });
+
+   
+        $("#development").click(function(){
+            $("#developmentP").show();
+            $("#development").hide();
+        });
+
+        $("#developmentP").click(function(){
+            $("#development").show();
+            $("#developmentP").hide();
+        
+    });
+
+
+
+
+
+})
